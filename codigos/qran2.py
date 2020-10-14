8338bc5fd80f60faadacbd57f86e746587726f4d
Luis Biedma <lbiedma17@gmail.com>
2020-10-14 09:05:35 -0300

    nuevos archivos

--- codigos/qran2.py
+++ codigos/qran2.py
@@ -22,6 +22,36 @@ def givens(x1,x2):
     return c, s
 
 
+def house(x):
+    '''
+    u, rho = house(x)
+    Calcula u y rho tal que Q = I - rho u u^T
+    cumple Qx = \|x\|_2 e^1
+    '''
+    n = len(x)
+    rho = 0
+    u = x.copy()
+    u[0] = 1.
+
+    if n == 1:
+        sigma = 0
+    else:
+        sigma = np.sum(x[1:]**2)
+
+    if sigma>0 or x[0]<0:
+        mu = np.sqrt(x[0]**2 + sigma)
+        if x[0]<=0:
+            gamma = x[0] - mu
+        else:
+            gamma = -sigma/(x[0] + mu)
+
+        rho = 2*gamma**2/(gamma**2 + sigma)
+        u = u/gamma
+        u[0] = 1
+
+    return u, rho
+
+
 def qrgivens(A):
     m, n = A.shape
     p = min(m-1, n)
@@ -40,4 +70,50 @@ def qrgivens(A):
         Q[:, m] = -Q[:, m]
 
     return Q, R
+
+
+def qrhouseholder(A):
+    m, n = A.shape
+    Q = np.eye(m)
+    p = min(m, n)
+    R = A.copy()
+    for jdx in range(p):
+        u, rho = house(R[jdx:, jdx])
+        w = rho * u
+        R[jdx:, jdx:] = R[jdx:, jdx:] - np.outer(w, u @ R[jdx:, jdx:])
+        Q[:, jdx:] = Q[:, jdx:] - Q[:, jdx:] @ np.outer(w, u)
+    
+    return Q, R
+
+
+def qrhholderp(A0):
+    '''
+    Q, R, P = qrhholderp(A)
+    Realiza la descomposición QR con permutaciones de A
+    utilizando reflexiones de Householder,
+    obteniendo que AP = QR
+    '''
+    A = A0.copy()
+    A = A.astype('float64')
+    m, n = A.shape
+    Q = np.eye(m)
+    P = np.eye(n)
+    c = np.sum(A**2, 0)
+    p = min([m,n])
+    for j in range(p):
+        l = np.argmax(c[j:])
+        l = j + l
+        if c[l]==0:
+            return Q, A, P
+        else:
+            A[:,[j,l]] = A[:,[l,j]]
+            P[:,[j,l]] = P[:,[l,j]]
+            c[[j,l]] = c[[l,j]]
+            u, rho = house(A[j:, j])
+            w = rho*u
+            A[j:, j:] = A[j:, j:] - np.outer(w, u.T @ A[j:, j:])
+            Q[:, j:] = Q[:, j:] - np.outer(Q[:, j:] @ w, u)
+            c[j:] = c[j:] - A[j, j:]**2
+
+    return Q, A, P
 
